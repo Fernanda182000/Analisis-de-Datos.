7c5c4c036b0bb76209f4f35070c8c0dd1370dd63
Fernanda182000 <maria.zamudio.ttv.278@unilibre.edu.co>
2024-11-06 22:23:33 -0500

    Creación Tabla Categorías

--- Laboratorio_4.sql
+++ Laboratorio_4.sql
@@ -7,3 +7,10 @@ CREATE TABLE Autores (
     Nacionalidad VARCHAR (100),
     Fecha_Nacimiento DATE
     );
+
+USE Biblioteca;
+CREATE TABLE Categorías (
+  ID_Categoria INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR (100) NOT NULL,
+    Categoria VARCHAR (100)
+    );
