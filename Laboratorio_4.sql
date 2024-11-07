fca00ed3e1b5ef78522f291132ccf28dbe9a3da0
Fernanda182000 <maria.zamudio.ttv.278@unilibre.edu.co>
2024-11-06 22:11:46 -0500

    Creación Tabla Autores

--- Laboratorio_4.sql
+++ Laboratorio_4.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE Biblioteca;
+
+USE Biblioteca;
+CREATE TABLE Autores (
+  ID_Autor INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR (100) NOT NULL,
+    Nacionalidad VARCHAR (100),
+    Fecha_Nacimiento DATE
+    );
