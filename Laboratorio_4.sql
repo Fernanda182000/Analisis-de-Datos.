ba04181c7949304d7c151c2517713d87a2d9d77f
Fernanda182000 <maria.zamudio.ttv.278@unilibre.edu.co>
2024-11-06 22:42:01 -0500

    Creación Tabla Libros

--- Laboratorio_4.sql
+++ Laboratorio_4.sql
@@ -14,3 +14,12 @@ CREATE TABLE Categorías (
     Nombre VARCHAR (100) NOT NULL,
     Categoria VARCHAR (100)
     );
+
+CREATE TABLE Libros (
+  ID_Libro INT PRIMARY KEY AUTO_INCREMENT,
+    Titulo VARCHAR (200) NOT NULL,
+    ID_Autor VARCHAR (100),
+    ID_Categoria INT, 
+    Año_Publicacion DATE,
+    Disponible VARCHAR (50)
+    );
