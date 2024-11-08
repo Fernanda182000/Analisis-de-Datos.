21773a6cdb4ac6390aba8fd3d020713520798af1
Fernanda182000 <maria.zamudio.ttv.278@unilibre.edu.co>
2024-11-07 23:08:18 -0500

    Adición de registros libros-usuarios-prestamos

--- Laboratorio_4.sql
+++ Laboratorio_4.sql
@@ -56,3 +56,19 @@ VALUES ('Novela'),
 ('Ciencia Ficción'), 
 ('Fantasía'),
 ('Ensayo');
+
+USE Biblioteca;
+INSERT INTO libros (Titulo, ID_Autor, ID_Categoria, Año_Publicacion)
+VALUES ('Cien Años de Soledad','1','1','1967'),
+('Harry Potter y la Piedra Filosofal','2','3','1997'),
+('La  era de 1984','3','2','1949'); 
+
+
+INSERT INTO usuarios (Nombre, Direccion, Telefono)
+VALUES ('Carlos Martínez','Calle Luna, 123','123456789'),
+('Lucía Fernández','Avenida Sol, 456','987654321'); 
+
+
+INSERT INTO prestamos (ID_Usuario, ID_Libro, Fecha_Prestamo, Fecha_Devolucion)
+VALUES ('1','1','2024-08-01','2024-08-15'), 
+('2','2','2024-08-02','2024-08-16');
