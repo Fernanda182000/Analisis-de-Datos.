e9574246aab801fd2145d288da9e669042b0bfb6
Fernanda182000 <maria.zamudio.ttv.278@unilibre.edu.co>
2024-11-11 20:02:51 -0500

    Creación tablas y los campos

--- Laboratorio_5.sql
+++ Laboratorio_5.sql
@@ -1,1 +1,16 @@
 CREATE DATABASE empresa
+
+USE empresa;
+CREATE TABLE Empleados (
+    ID_Empl INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR (100) NOT NULL,
+    Cargo VARCHAR (50),
+    Salario DECIMAL (10,2)
+    );
+    
+    USE empresa;
+CREATE TABLE Cargo (
+    ID_Cargo INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre_cargo VARCHAR (50) NOT NULL,
+    Salario DECIMAL (10, 2)
+    );
